9a4aaa803a292292846d812fb777307bc5b29b96
hhh217 <2848589350@qq.com>
2025-04-15 09:48:37 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'./',
+  base:'/github.io/',
   plugins: [
     vue(),
     AutoImport({
