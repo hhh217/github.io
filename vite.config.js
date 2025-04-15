662c913d1b78ff662c6c72c0ffb6f7ffd10d50ba
hhh217 <2848589350@qq.com>
2025-04-15 09:26:17 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/gh-pages/",
+  base: "/*",
   plugins: [
     vue(),
     AutoImport({
